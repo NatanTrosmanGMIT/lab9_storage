db815fc180816bbf19a972a6caf920bd2d9c8b70
Natan Trosman <you@example.comG00347770@gmit.ie>
2021-04-21 15:15:06 +0100

    lab complete

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  myStatus: string;
+  constructor(private storage: Storage) { }
+
+  ionViewDidEnter() {
+
+    this.storage.get("status")
+      .then((data) => {
+        this.myStatus = data;
+      })
+      .catch();
+  }
+}
